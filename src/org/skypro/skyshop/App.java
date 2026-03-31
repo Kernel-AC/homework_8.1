679edec84f5000657ceafa169b6ce79ffab91400
Kernel-AC <Kernel.AC@yandex.ru>
2026-04-01 01:30:49 +0300

    newCommit

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,43 +1,44 @@
 package org.skypro.skyshop;
 
 import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.DiscountedProduct;
+import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.Product;
+import org.skypro.skyshop.product.SimpleProduct;
 
 public class App {
     public static void main(String[] args) {
-        Product product1 = new Product("Яблоко", 50);
-        Product product2 = new Product("Банан", 150);
-        Product product3 = new Product("Груша", 30);
-        Product product4 = new Product("Ананас", 200);
-        Product product5 = new Product("Слива", 500);
-        Product product6 = new Product("Мандарин", 350);
+        Product apples = new SimpleProduct("Яблоки", 150);
+        Product pears = new SimpleProduct("Груши", 200);
+        Product oranges = new FixPriceProduct("Апельсины");
+        Product bananas = new FixPriceProduct("Бананы");
+        Product lemons = new DiscountedProduct("Лимоны", 150, 20);
+        Product kiwi = new DiscountedProduct("Киви", 250, 35);
+
 
         ProductBasket basket = new ProductBasket();
 
-        // 1.добавление продукта в корзину
-        basket.addProduct(product1);
-        basket.addProduct(product2);
-        basket.addProduct(product3);
-        basket.addProduct(product4);
-        basket.addProduct(product6);
-        System.out.println();
+        // добавление разных типов товаров в корзину
+        basket.addProduct(apples);
+        basket.addProduct(pears);
+        basket.addProduct(oranges);
+        basket.addProduct(bananas);
+        basket.addProduct(lemons);
 
-        //2.добавление в заполненную корзину, в которой нет места
-        for (int i = 0; i < 2; i++) {
-            basket.addProduct(product5);
-        }
+        // добавление продукта в заполненую корзину
+        basket.addProduct(kiwi);
 
-        //3.печать содержимого корзины с несколькими товарами
+        // печать содержимого корзины с несколькими товарами
         basket.printContents();
 
-        // 4.получение стоимости корзины с несколькими товарами
-        System.out.println("Стоимость корзины: " + basket.getTotalPrice());
+        // получение стоимости корзины с несколькими товарами
+        System.out.println("Стоимость корзины: " + basket.getTotalCost());
 
-        // 5.поиск товара, который есть в корзине
-        System.out.println("Есть ли «Яблоко» в корзине? " + basket.containsProduct("Яблоко"));
+        // поиск товара, который есть в корзине
+        System.out.println("Есть ли «Яблоки» в корзине? " + basket.containsProduct("Яблоки"));
 
         // 6.поиск товара, которого нет в корзине
-        System.out.println("Есть ли «Морковь» в корзине? " + basket.containsProduct("Морковь"));
+        System.out.println("Есть ли «Киви» в корзине? " + basket.containsProduct("Киви"));
 
         // 7.очистка корзины
         basket.clearBasket();
@@ -46,10 +47,10 @@ public class App {
         basket.printContents();
 
         // 9.получение стоимости пустой корзины
-        System.out.println("Стоимость пустой корзины: " + basket.getTotalPrice());
+        System.out.println("Стоимость пустой корзины: " + basket.getTotalCost());
 
         // 10.поиск товара по имени в пустой корзине
-        System.out.println("Есть ли «Груша» в пустой корзине? " + basket.containsProduct("Груша"));
+        System.out.println("Есть ли «Груши» в пустой корзине? " + basket.containsProduct("Груша"));
 
     }
 }
